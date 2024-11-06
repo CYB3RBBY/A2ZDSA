a2d86a98502c7c35077a5b6e59c6a79164928d1e
CYB3RBBY <aishwaryadhyani04@gmail.com>
2024-11-06 13:06:28 +0530

    add changes

---
+++ ifelse.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+// Write a program that takes an input of age
+// and prints if you are adult or not.
+// int main() {
+//     int age;
+//     cout << "Enter your age: ";
+//     cin >> age;
+//     if (age >= 18) {
+//         cout << "You are an adult." << endl;
+//     }
+//     else {
+//         cout << "You are not an adult." << endl;
+//     }
+//     return 0;
+// }
+
+/*
+A school has following rules for grading system:
+below 25 -f 
+25 - 44 - E
+45 - 49 - D
+50 - 59 - C
+60 - 79 - B
+80 - 100 - A
+Ask user to enter marks and print the corresponding grade.
+ */
+int main(){
+    int marks;
+    cout << "Enter your marks: ";
+    cin >> marks;
+    if (marks = 100 && marks >= 80){
+        cout << "Congratulations! You have secured grade A." << endl;
+    }
+    else if (marks < 80 && marks >= 60){
+        cout << "You have secured grade B." << endl;
+    }
+    else if (marks < 60 && marks >= 50){
+        cout << "You have secured grade C." << endl;
+    }
+    else if (marks < 50 && marks >= 45){
+        cout << "You have secured grade D." << endl;
+    }
+    else if (marks < 45 && marks >= 25){
+        cout << "You have secured grade E." << endl;
+    }
+    else{
+        cout << "Invalid grade." << endl;
+    }
+    return 0 ;
+}
