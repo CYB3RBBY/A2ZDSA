87fcf95020258e0f14d762572cacd2ac2e5c0a20
CYB3RBBY <aishwaryadhyani04@gmail.com>
2024-11-06 12:19:36 +0530

    add changes

--- first.cpp
+++ first.cpp
@@ -1,4 +1,15 @@
-#include <iostream>
+
+// common skeleton for cpp
+
+// #include<bits/stdc++.h> 
+// using namespace std;
+// int main(){
+//     return 0;
+// }
+
+
+#include<bits/stdc++.h> // includes all the libraries.
+// #include <iostream>
 using namespace std;
 
 // hello world 
@@ -12,9 +23,30 @@ using namespace std;
 
 // cin 
 
+// int main(){
+//     int x, y;
+//     cin >> x >> y;
+//     cout << "You entered x: " << x << " and y:" <<y;
+//     return 0;
+// }
+
+// integer datatype = int 4 bytes
+// long = 4 bytes
+// long long = 8 bytes
+// float , double 
+
 int main(){
-    int x, y;
-    cin >> x >> y;
-    cout << "You entered x: " << x << " and y:" <<y;
+    int x = 10;
+    long y = 15;
+    long long z = 15000000000000;
+    float a = 1.5;
+    float b = 9.9;
+    double c = 1.56;
+    cout << "value of x is " << x <<"\n";
+    cout << "value of y is " << y <<"\n";
+    cout << "value of z is " << z <<"\n";
+    cout << "value of a is " << a <<"\n";
+    cout << "value of b is " << b <<"\n";
+    cout << "value of c is " << c <<"\n";
     return 0;
 }
