c4268c829df0fd3272cdf5de61cb6343cca7d801
CYB3RBBY <aishwaryadhyani04@gmail.com>
2024-11-06 10:05:53 +0530

    add changes

---
+++ first.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+// hello world 
+
+// int main(){
+//    cout << "Hey Kazehaya!" << "\n" << "Hey Sawako!" << "\n";
+//    cout << "Hey Ryu!";
+
+//    return 0;
+// }
+
+// cin 
+
+int main(){
+    int x;
+    cin >> x;
+    cout << "You entered: " << x;
+    return 0;
+}
