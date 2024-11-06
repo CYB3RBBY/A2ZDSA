c325fb905d07f02beed76f3b440c8bd903b1ff3b
CYB3RBBY <aishwaryadhyani04@gmail.com>
2024-11-06 13:55:12 +0530

    add changes

--- ifelse.cpp
+++ ifelse.cpp
@@ -63,5 +63,20 @@ retirement time
 */
 
 int main(){
-    
+    int age;
+    cout << "Enter your age: ";
+    cin >> age;
+    if(age < 18){
+        cout << "Not eligible for job." << endl;
+    }
+    else if(age >= 18 && age <= 54){
+        cout << "Eligible for job." << endl;
+    }
+    else if(age >= 55 && age <= 57){
+        cout << "Eligible for job, but retirement soon." << endl;
+    }
+    else{
+        cout << "Retirement time." << endl;
+    }
+    return 0;
 }
