e1f0df2ae03b4d129413442884c6f2d707ec9bfa
CYB3RBBY <aishwaryadhyani04@gmail.com>
2024-11-06 19:54:35 +0530

    add changes

---
+++ ArraysAndStrings.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// int main(){
+//     // 2d array
+//     int arr[3][5];
+    
+//     arr[1][3] = 68;
+//     cout << arr[1][3];
+//     return 0;
+// }
+
+// int main(){
+//     string s = "Aishwarya";
+//     int len = s.size();
+//     s[len-1] = 'x';
+//     cout << s[len-1];
+//     return 0;
+// }
