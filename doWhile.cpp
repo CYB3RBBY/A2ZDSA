04bed80b3889031ee9c55fb72f5690c1c2137644
CYB3RBBY <aishwaryadhyani04@gmail.com>
2024-11-07 11:04:36 +0530

    add files

---
+++ doWhile.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int i = 2;
+    do {
+        cout << "aishwarya" << i << endl;
+        i = i + 1;
+    } while (i <=1);
+    return 0;
+}
