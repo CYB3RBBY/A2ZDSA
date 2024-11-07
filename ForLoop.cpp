3c607cb11a11f2539b3a16ecc486933c3b26de05
CYB3RBBY <aishwaryadhyani04@gmail.com>
2024-11-07 10:56:42 +0530

    add changes

---
+++ ForLoop.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // for (int i = 1; i <=10; i++){
+    // cout << "aishu" << endl;
+    // }
+    int i;
+    for (int i = 5; i>0; i--){
+    cout << "aishu" << endl;
+    }
+    return 0;
+}
