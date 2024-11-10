2d129385b6517038ea3782d5f43a041d3bcc9ee4
CYB3RBBY <aishwaryadhyani04@gmail.com>
2024-11-10 13:12:22 +0530

    add changes patterns

---
+++ 1_LearnTheBasics/Lecture2/Pattern1.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Pattern 1
+
+int main(){
+    for(int i = 0; i < 5; i++){
+        for (int j = 0; j < 5; j++) {
+            cout << "*" ;
+        }
+        cout << endl;
+    }
+}
