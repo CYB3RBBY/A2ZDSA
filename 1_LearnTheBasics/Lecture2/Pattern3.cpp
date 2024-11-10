cf349dc7d6a7edc5ab0d19c4fef675468bd2952b
CYB3RBBY <aishwaryadhyani04@gmail.com>
2024-11-10 13:43:17 +0530

    add changes

---
+++ 1_LearnTheBasics/Lecture2/Pattern3.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Pattern 3
+
+int main(){
+    for(int i = 0; i < 6; i++){
+       for(int j = 0; j<= i; j++){
+        cout << j + 1;
+       }
+       cout << endl;
+    }
+    return 0;
+}
