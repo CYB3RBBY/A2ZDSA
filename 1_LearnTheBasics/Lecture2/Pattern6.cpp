d816607875f878ea467dfebb8841485cfffd42dc
CYB3RBBY <aishwaryadhyani04@gmail.com>
2024-11-11 18:00:53 +0530

    add changes

--- 1_LearnTheBasics/Lecture2/Pattern6.cpp
+++ 1_LearnTheBasics/Lecture2/Pattern6.cpp
@@ -4,10 +4,10 @@ using namespace std;
 // Pattern 6
 
 int main () {
-    for (int i = 0; i < 5; i++){
-        for (int j = 5; j>i; j--)
+    for (int i = 0; i < 5; i++){ // i determines number of rows
+        for (int j = 5; j>i; j--) // j determines number of columns 
         {
-            cout << 5-j+1 <<" ";
+            cout << 5 - j + 1 <<" "; // this line determines the numbers which are gonna be printed line wise
         }
         cout << endl;
     }
