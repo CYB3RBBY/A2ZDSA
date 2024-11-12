033c6f18d22a74483e3250fc9afbfdb2e884f4ca
CYB3RBBY <aishwaryadhyani04@gmail.com>
2024-11-12 16:41:47 +0530

    add changes

---
+++ 1_LearnTheBasics/Lecture2/Pattern14.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// pattern 14
+
+void pattern14(int n){
+   for (int i = 0; i < n; i++){
+    for (char c = 'A'; c <= 'A' + i; c++){
+        cout << c<< " ";
+    }
+    cout << endl;
+   }
+}
+
+int main() {
+    int n = 5; 
+    pattern14(n);
+    return 0;
+}
