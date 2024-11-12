e772f1a30d22ab372507d718546324c60dd17744
CYB3RBBY <aishwaryadhyani04@gmail.com>
2024-11-12 18:26:23 +0530

    add pattern 15

---
+++ 1_LearnTheBasics/Lecture2/Pattern15.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Pattern 15
+
+void pattern15(int n){
+   for (int i = 0; i < n; i++){
+    for(char ch = 'A'; ch<='A'+(n-i-1);ch++){
+        cout<<ch<<" ";
+              
+    }
+    cout << endl;
+   }
+}
+
+int main(){
+    int n = 5;
+    pattern15(n);
+    return 0;
+}
